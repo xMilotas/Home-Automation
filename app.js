5d26937b8e3a0fa37916cb1b011e89f30a9a1f81
xMilotas <pascalkaspar@web.de>
2017-10-25 21:52:14 +0200

    First Commit

---
+++ app.js
@@ -0,0 +1,33 @@
+
+// Load all required modules
+var express = require('express');
+var apiRoutes = require('./api/api');
+var bodyParser = require('body-parser')
+var app = express()
+
+// Use bodyParser to send JSON
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+//Allow Cross-Domain-Requests
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+//Make Public directory accessible
+app.use(express.static(__dirname + '/public'));
+
+//Routing directories
+app.use('/api', apiRoutes);
+
+// Serve index.html for all Calls except API ones - Even when pages are not supported or errors occur
+app.get('*', function(req, res){
+	res.sendFile(__dirname + '/public/index.html');
+});
+
+var port = 80;
+app.listen(port, function() {
+    console.log('Server runnning on Port: ' + port);
+});
