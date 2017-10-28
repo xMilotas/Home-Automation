8b753ac738d445f4dc80298852a32981aaeea7b4
xMilotas <pascalkaspar@web.de>
2017-10-28 12:21:48 +0200

    Updated Plugcodes

--- api/api.js
+++ api/api.js
@@ -1,56 +1,59 @@
 var express = require('express');
 var router = express.Router();
 var url = require('url');
-const { exec } = require('child_process');
+const exec = require('child_process');
 module.exports = router;
 
 
+router.post('/PowerPlugs', function(req, res, next) {
+  try {
+    console.log(req.body);
+    var reqObj = req.body;
+    var outletID = reqObj.outletID;
+    var outletStatus = reqObj.outletStatus;
+    if (outletID == '6') {
+      for (var i = 1; i < 5; i++) {
+        setTimeout(function() {
+          res.send(sendCodes(readCodes(i, outletStatus)));
+        }, 500);
+      }
+    }
 
-
-
-router.post('/PowerPlugs', function(req, res, next){
-	try{
-		console.log(req.body);
-		var reqObj= req.body;
-		var outletID = reqObj.outletID;
-		var outletStatus = reqObj.outletStatus;
-
-		console.log("ID: "+outletID+" Status: "+outletStatus);
-
-		res.send(sendCodes(readCodes(outletID, outletStatus)));
-
-	} catch(ex)
-	{
-		console.error("Internal error:"+ex);
-		return next(ex);
-	}
-});
-
-function sendCodes(plugcode){
-	var command ="sudo ./codesend "+plugcode+" -l 184";
-console.log(command);
-exec(command, function (error, stdout, stderr){
-  if (error !== null) {
-    console.log('exec error: ' + error);
+    res.send(sendCodes(readCodes(outletID, outletStatus)));
+  } catch (ex) {
+    console.error("Internal error:" + ex);
+    return next(ex);
   }
 });
-	return('Success');
+
+function sendCodes(plugcode) {
+  var command = "sudo ./codesend " + plugcode + " -l 184";
+  exec(command, function(error, stdout, stderr) {
+    if (error !== null) {
+      console.log('exec error: ' + error);
+    }
+  });
+  return ('Success');
 }
 
 //TODO Use MongoDB for PowerOutletCodes
-function readCodes(outletID, outletStatus){
-	var powerOnMap = {
-		'1': '4478259',
-		'2': '4478403'
-	}
+function readCodes(outletID, outletStatus) {
+  var powerOnMap = {
+    '1': '4478259',
+    '2': '4478403',
+    '3': '4478723',
+    '4': '4480259',
+    '5': '4486403'
+  }
 
-	var powerOffMap = {
-		'1': '4478268',
-		'2': '4478412'
-	}
-
-	if (outletStatus = 0) plugcode = powerOffMap[outletID];
-	if (outletStatus = 1) plugcode = powerOnMap[outletID];
-
-	return plugcode;
+  var powerOffMap = {
+    '1': '4478268',
+    '2': '4478412',
+    '3': '4478732',
+    '4': '4480268',
+    '5': '4486412'
+  }
+  plugcode = powerOffMap[outletID];
+  if (outletStatus == 1) plugcode = powerOnMap[outletID];
+  return plugcode;
 }
