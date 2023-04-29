d2dda8b0f89a998dc48cbee85143d130adf5b27e
Pascal <pascalkaspar@tutanota.com>
2023-04-29 11:21:43 +0200

    Enable running headless

--- api/whatsapp_client.js
+++ api/whatsapp_client.js
@@ -3,15 +3,20 @@ const qrcode = require('qrcode-terminal');
 const { Client, LocalAuth } = require('whatsapp-web.js');
 
 const client = new Client({
-  authStrategy: new LocalAuth()
+    authStrategy: new LocalAuth(),
+    puppeteer: {
+        args: ['--no-sandbox'],
+    }
 });
 
 client.on('loading_screen', (percent, message) => {
-  console.log('LOADING SCREEN', percent, message);
+    console.log('LOADING SCREEN', percent, message);
 });
 
 client.on('qr', qr => {
-    qrcode.generate(qr, {small: true});
+    qrcode.generate(qr, {
+        small: true
+    });
 });
 
 client.on('ready', () => {
@@ -19,8 +24,8 @@ client.on('ready', () => {
 });
 
 client.on('auth_failure', msg => {
-  // Fired if session restore was unsuccessful
-  console.error('AUTHENTICATION FAILURE', msg);
+    // Fired if session restore was unsuccessful
+    console.error('AUTHENTICATION FAILURE', msg);
 });
 
 client.initialize()
